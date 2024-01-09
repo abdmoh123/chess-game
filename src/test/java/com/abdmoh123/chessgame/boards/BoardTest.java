ed0f595a88cb1834f618b5ed4cc934dae773e588
abdmoh123 <abdhawisa@gmail.com>
2024-01-09 23:15:32 +0000

    Add missing changes from previous commit

---
+++ src/test/java/com/abdmoh123/chessgame/boards/BoardTest.java
@@ -0,0 +1,28 @@
+package com.abdmoh123.chessgame.boards;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.abdmoh123.chessgame.pieces.Piece;
+import com.abdmoh123.chessgame.utils.ChessCSVReader;
+
+import org.junit.Assert;
+
+public class BoardTest {
+    Piece[][] expected_standard_board_contents;
+
+    @Before
+    public void init() throws Exception {
+        expected_standard_board_contents = ChessCSVReader.readBoardCSV(
+            "board_layouts/standard_board_layout.csv"
+        );
+    }
+
+    @Test
+    public void standardBoardSmokeTest() {
+        Board standard_board = new StandardBoard();
+        standard_board.initialise();
+
+        Assert.assertArrayEquals(expected_standard_board_contents, standard_board.getAllSpaces());
+    }
+}
