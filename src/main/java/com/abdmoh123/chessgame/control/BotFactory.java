8b8e71f4ef8ceac5d43da18da617216c8094a0be
abdmoh123 <abdhawisa@gmail.com>
2024-01-18 13:55:28 +0000

    Rename type parameter in BotFactory createBot to bot_type_in * Implement a basic test for BotPlayerTest ensuring actual time taken to move < double the given time

--- src/main/java/com/abdmoh123/chessgame/control/BotFactory.java
+++ src/main/java/com/abdmoh123/chessgame/control/BotFactory.java
@@ -1,8 +1,8 @@
 package com.abdmoh123.chessgame.control;
 
 public class BotFactory {
-    public static BotPlayer createBot(BotType type, boolean is_white_in) {
-        if (type == BotType.RANDOM_BOT) { return new RandomBot(is_white_in); }
+    public static BotPlayer createBot(BotType bot_type_in, boolean is_white_in) {
+        if (bot_type_in == BotType.RANDOM_BOT) { return new RandomBot(is_white_in); }
         return null;
     }
 }
--- src/test/java/com/abdmoh123/chessgame/control/BotPlayerTest.java
+++ src/test/java/com/abdmoh123/chessgame/control/BotPlayerTest.java
@@ -1,11 +1,42 @@
 package com.abdmoh123.chessgame.control;
 
+import com.abdmoh123.chessgame.boards.Board;
+import com.abdmoh123.chessgame.boards.StandardBoard;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BotPlayerTest {
+    List<BotPlayer> bot_list = new ArrayList<>();
+    Board chess_board = new StandardBoard();
+
     @Before
     public void init() {
+        for (BotType bot_type : BotType.values()) {
+            bot_list.add(BotFactory.createBot(bot_type, true));
+        }
+        chess_board.initialise();
+    }
+
+    @Test
+    public void testThinkingTime() {
+        for (BotPlayer bot : bot_list) {
+            if (bot.getThinkingTime() < 0) {
+                Assert.fail("Time taken to think cannot be negative!");
+            }
+            else if (bot.getThinkingTime() > 0) {
+                long start_time = System.currentTimeMillis();
+                bot.startMove(chess_board);
+                long end_time = System.currentTimeMillis();
+                long time_taken = end_time - start_time;
+
+                // actual time taken should never be greater than double the given thinking time (leeway added)
+                Assert.assertTrue(time_taken < 2 * bot.getThinkingTime());
+            }
+        }
     }
 }
