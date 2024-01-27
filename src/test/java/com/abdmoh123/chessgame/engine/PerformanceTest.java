e2c78e161b82b1bb685ec68d5048c44839f9cfb5
abdmoh123 <abdhawisa@gmail.com>
2024-01-27 16:44:57 +0000

    Create runPerfTestDivide method (useful for debugging) * Rename runPerfTest current_depth to depth_in * Refactor Docstring for runPerfTest

--- src/test/java/com/abdmoh123/chessgame/engine/PerformanceTest.java
+++ src/test/java/com/abdmoh123/chessgame/engine/PerformanceTest.java
@@ -50,16 +50,16 @@ public class PerformanceTest {
         this.players = new Player[]{new RandomBot(true), new RandomBot(false)};
     }
 
-    private int[] runPerfTest(int current_depth) {
-        /* Count all possible legal moves (leaf nodes of move generation tree) of a given depth (number of moves) */
+    private int[] runPerfTest(int depth_in) {
+        /* Count all possible combinations of legal moves of a given depth */
 
         // update node count
-        if (current_depth == 0) {
+        if (depth_in == 0) {
             return new int[]{1, 0, 0};
         }
 
         Player current_player;
-        if ((this.depth - current_depth) % 2 == 0) {
+        if ((this.depth - depth_in) % 2 == 0) {
             current_player = this.players[0];
         }
         else {
@@ -79,7 +79,7 @@ public class PerformanceTest {
 
                 this.standard_board = this.standard_board.after(move);
 
-                int[] results_array = runPerfTest(current_depth - 1);
+                int[] results_array = runPerfTest(depth_in - 1);
                 nodes += results_array[0];
                 captures += results_array[1];
                 checks += results_array[2];
@@ -89,6 +89,65 @@ public class PerformanceTest {
         }
 
         return new int[]{nodes, captures, checks};
+    }
+
+    private void displayDivideResults(List<Move> moves, List<Integer> perft_results) {
+        // if depth = 1, output all the moves with node results = 0
+        if (perft_results == null) {
+            for (Move move : moves) {
+                String move_name = move.getNotation(false, true);
+                System.out.printf("%s: 0\n", move_name);
+            }
+            return;
+        }
+
+        if (moves.size() != perft_results.size()) {
+            System.out.println("Error! Lists are unequal in length!");
+        }
+
+        // output format example: aa3: 20
+        for (int i = 0; i < moves.size(); ++i) {
+            String move_name = moves.get(i).getNotation(false, true);
+            int num_nodes = perft_results.get(i);
+            System.out.printf("%s: %d\n", move_name, num_nodes);
+        }
+    }
+    private void runPerfTestDivide(int depth_in) {
+        /* List all moves (at depth = 1) and for each move, output the perft result at the given depth. Useful for debugging. */
+
+        if (depth_in < 1) {
+            System.out.println("Depth too low! Must be > 1");
+            return;
+        }
+
+        Player current_player;
+        if ((this.depth - depth_in) % 2 == 0) {
+            current_player = this.players[0];
+        }
+        else {
+            current_player = this.players[1];
+        }
+
+        List<Space> friendly_spaces = this.standard_board.getFriendlySpaces(current_player.isWhite());
+        List<Move> legal_moves = new ArrayList<>();
+        for (Space space : friendly_spaces) {
+            legal_moves.addAll(current_player.getLegalMoves(space, this.standard_board));
+        }
+
+        // print header of test results
+        System.out.println("Divide test depth " + depth_in);
+
+        if (depth_in == 1) {
+            displayDivideResults(legal_moves, null);
+        }
+
+        List<Integer> nodes_list = new ArrayList<>();
+        for (Move move : legal_moves) {
+            this.standard_board = this.standard_board.after(move);
+            nodes_list.add(runPerfTest(depth_in - 1)[0]); // 0 index = number of possible move combinations
+            this.standard_board = this.standard_board.before(move);
+        }
+        displayDivideResults(legal_moves, nodes_list);
     }
 
     @Test
@@ -103,6 +162,9 @@ public class PerformanceTest {
             actual_results[1], expected_results[1],
             actual_results[2], expected_results[2]
         );
+
+        runPerfTestDivide(depth);
+
         Assert.assertArrayEquals(expected_results, actual_results);
     }
 }
