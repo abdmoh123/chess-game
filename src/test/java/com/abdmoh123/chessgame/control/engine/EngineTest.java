48fcb5aed47e01ccd24dafa7290e1ba5b52a6c6a
abdmoh123 <abdhawisa@gmail.com>
2024-01-29 22:38:22 +0000

    Refactor test functions in EngineTest (less clutter)

--- src/test/java/com/abdmoh123/chessgame/control/engine/EngineTest.java
+++ src/test/java/com/abdmoh123/chessgame/control/engine/EngineTest.java
@@ -4,11 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
-import com.abdmoh123.chessgame.Game;
 import com.abdmoh123.chessgame.boards.Board;
 import com.abdmoh123.chessgame.boards.StandardBoard;
-import com.abdmoh123.chessgame.control.Human;
-import com.abdmoh123.chessgame.control.Player;
 
 public class EngineTest {
     @Before
@@ -21,11 +18,10 @@ public class EngineTest {
             Board test_board = new StandardBoard();
             test_board.initialise("board_layouts/game_test/checkmate_true.csv");
 
-            Player[] players = {new Human(true), new Human(false)};
-            Game chess_game = new Game(players, test_board);
+            Engine chess_engine = new Engine(test_board);
 
             // inputted layout should be checkmate (king cannot move)
-            boolean is_check_mate = chess_game.getEngine().isCheckMate(players[0].isWhite());
+            boolean is_check_mate = chess_engine.isCheckMate(true);
             Assert.assertTrue(is_check_mate);
         }
         catch (Exception e) {
@@ -40,11 +36,10 @@ public class EngineTest {
             Board test_board = new StandardBoard();
             test_board.initialise("board_layouts/game_test/checkmate_false.csv");
 
-            Player[] players = {new Human(true), new Human(false)};
-            Game chess_game = new Game(players, test_board);
+            Engine chess_engine = new Engine(test_board);
 
             // inputted layout should not be checkmate (king can take queen)
-            boolean is_check_mate = chess_game.getEngine().isCheckMate(players[0].isWhite());
+            boolean is_check_mate = chess_engine.isCheckMate(true);
             Assert.assertFalse(is_check_mate);
         }
         catch (Exception e) {
@@ -59,11 +54,10 @@ public class EngineTest {
             Board test_board = new StandardBoard();
             test_board.initialise("board_layouts/game_test/stalemate_true.csv");
 
-            Player[] players = {new Human(true), new Human(false)};
-            Game chess_game = new Game(players, test_board);
+            Engine chess_engine = new Engine(test_board);
 
             // inputted layout should be stalemate (white king cannot move)
-            boolean is_stalemate = chess_game.getEngine().isStalemate(players[0].isWhite());
+            boolean is_stalemate = chess_engine.isStalemate(true);
             Assert.assertTrue(is_stalemate);
         }
         catch (Exception e) {
@@ -78,11 +72,10 @@ public class EngineTest {
             Board test_board = new StandardBoard();
             test_board.initialise("board_layouts/game_test/stalemate_false.csv");
 
-            Player[] players = {new Human(true), new Human(false)};
-            Game chess_game = new Game(players, test_board);
+            Engine chess_engine = new Engine(test_board);
 
             // inputted layout should not be stalemate (white king can move)
-            boolean is_stalemate = chess_game.getEngine().isStalemate(players[0].isWhite());
+            boolean is_stalemate = chess_engine.isStalemate(true);
             Assert.assertFalse(is_stalemate);
         }
         catch (Exception e) {
