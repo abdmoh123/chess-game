a73be2223f82c4e03a3134ff9ebe83b41d1e88f6
abdmoh123 <abdhawisa@gmail.com>
2024-02-22 23:24:22 +0000

    Add JavaFX as dependency and plugin * Rewrite main class ChessGame.java to display an empty window

--- src/main/java/com/abdmoh123/chessgame/ChessGame.java
+++ src/main/java/com/abdmoh123/chessgame/ChessGame.java
@@ -2,23 +2,20 @@ package com.abdmoh123.chessgame;
 
 import java.util.Scanner;
 
-import com.abdmoh123.chessgame.boards.Board;
-import com.abdmoh123.chessgame.boards.StandardBoard;
-import com.abdmoh123.chessgame.control.Human;
-import com.abdmoh123.chessgame.control.Player;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class ChessGame {
+public class ChessGame extends Application {
     public static final Scanner SCANNER = new Scanner(System.in);
 
+    @Override
+    public void start(Stage primary_stage) throws Exception {
+        primary_stage.setTitle("Chess");
+
+        primary_stage.show();
+    }
+
     public static void main(String[] args) {
-        Player[] players = {new Human(true), new Human(false)};
-
-        Board chess_board = new StandardBoard();
-        chess_board.initialise();
-
-        Game chess_game = new Game(players, chess_board);
-        chess_game.runGame();
-
-        SCANNER.close();
+        launch(args);
     }
 }
