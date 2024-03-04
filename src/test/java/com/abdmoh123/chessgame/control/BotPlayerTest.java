1971a6ecb264b3105c1f6dd8deb9a9b2d9d701cf
abdmoh123 <abdhawisa@gmail.com>
2024-03-04 19:28:11 +0000

    Remove thinking time attribute and relevant methods and tests from BotPlayer class

--- src/test/java/com/abdmoh123/chessgame/control/BotPlayerTest.java
+++ src/test/java/com/abdmoh123/chessgame/control/BotPlayerTest.java
@@ -5,8 +5,6 @@ import com.abdmoh123.chessgame.boards.StandardBoard;
 import com.abdmoh123.chessgame.control.engine.Engine;
 
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,23 +21,5 @@ public class BotPlayerTest {
         Board chess_board = new StandardBoard();
         chess_board.initialise();
         chess_engine = new Engine(chess_board);
-    }
-
-    @Test
-    public void testThinkingTime() {
-        for (BotPlayer bot : bot_list) {
-            if (bot.getThinkingTime() < 0) {
-                Assert.fail("Time taken to think cannot be negative!");
-            }
-            else if (bot.getThinkingTime() > 0) {
-                long start_time = System.currentTimeMillis();
-                bot.startMove(chess_engine);
-                long end_time = System.currentTimeMillis();
-                long time_taken = end_time - start_time;
-
-                // actual time taken should never be greater than double the given thinking time (leeway added)
-                Assert.assertTrue(time_taken < 2 * bot.getThinkingTime());
-            }
-        }
     }
 }
--- src/test/java/com/abdmoh123/chessgame/pieces/PieceTest.java
+++ src/test/java/com/abdmoh123/chessgame/pieces/PieceTest.java
@@ -1,8 +1,6 @@
 package com.abdmoh123.chessgame.pieces;
 
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.Assert;
 
 public class PieceTest {
     @Before
